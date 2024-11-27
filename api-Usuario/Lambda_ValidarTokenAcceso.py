4c79d97f0b9e97113cc3ee380b0e34e9b604b8f9
Jordinn Martin Reyes Medina <113043632+jordinnreyes@users.noreply.github.com>
2024-11-27 14:29:11 -0500

    Update Lambda_ValidarTokenAcceso.py

--- api-Usuario/Lambda_ValidarTokenAcceso.py
+++ api-Usuario/Lambda_ValidarTokenAcceso.py
@@ -1,52 +1,58 @@
 import boto3
 from datetime import datetime
-
-import os  # Para acceder a las variables de entorno
+import os
+import json
 
 def lambda_handler(event, context):
     # Entrada (json)
     token = event['token']
-
     user_id_request = event.get('user_id')  
+    
     # Proceso
     dynamodb = boto3.resource('dynamodb')
     table_name = os.environ['t_tokens_acceso']  # Usa la variable de entorno
     table = dynamodb.Table(table_name)
 
-    response = table.get_item(
-        Key={
-            'token': token
+    try:
+        response = table.get_item(Key={'token': token})
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': f'Error al acceder a la base de datos: {str(e)}'
         }
-    )
+
     if 'Item' not in response:
         return {
             'statusCode': 403,
             'body': 'Token no existe'
         }
-    
+
     # Obtener el usuario asociado al token
     user_id = response['Item'].get('user_id')
     expires = response['Item']['expires']
-    now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    
-    if now > expires:
+
+    # Convierte 'expires' y 'now' a objetos datetime para compararlos
+    expires_datetime = datetime.strptime(expires, '%Y-%m-%d %H:%M:%S')
+    now_datetime = datetime.now()
+
+    if now_datetime > expires_datetime:
         return {
             'statusCode': 403,
             'body': 'Token expirado'
         }
-    
+
     # Validar que el user_id del token coincida con el user_id de la solicitud
     if user_id_request != user_id:
         return {
             'statusCode': 403,
             'body': 'El token no corresponde al usuario logueado'
         }
-    
+
     # Salida (json)
     return {
-    'statusCode': 200,
-    'body': json.dumps({
-        'message': 'Token válido',
-        'user_id': user_id
-    })
-}
+        'statusCode': 200,
+        'body': json.dumps({
+            'message': 'Token válido',
+            'user_id': user_id
+        })
+    }
