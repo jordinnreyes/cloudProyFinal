5cac10242c9f6f5963a4aca2c57edc40be33cbe4
Jordinn Martin Reyes Medina <113043632+jordinnreyes@users.noreply.github.com>
2024-11-27 13:13:33 -0500

    Update crearResenia.js

--- api-Resena/crearResenia.js
+++ api-Resena/crearResenia.js
@@ -1,76 +1,116 @@
-import boto3
-import hashlib
-import json
-import os  # Para acceder a las variables de entorno
+const AWS = require('aws-sdk');
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+const lambda = new AWS.Lambda();
+//const { DateTime } = require('luxon');
 
-# Hashear contraseña
-def hash_password(password):
-    # Retorna la contraseña hasheada
-    return hashlib.sha256(password.encode()).hexdigest()
+const { randomUUID } = require('crypto');
+//const uuid = require('uuid'); // Asegúrate de tener esta librería instalada
 
-# Función que maneja el registro de user y validación del password
-def lambda_handler(event, context):
-    try:
-        # Esto maneja el caso donde no haya un cuerpo válido
-        # Obtener el email y el password
-        body = json.loads(event.get('body', '{}'))
-        user_id = body.get('user_id')
-        password = body.get('password')
+const REVIEWS_TABLE = process.env.REVIEWS_TABLE;
+if (!REVIEWS_TABLE) {
+    throw new Error("La variable de entorno REVIEWS_TABLE no está configurada.");
+}
 
+exports.handler = async (event) => {
+
+    console.log("Contenido de event.body:", event.body);
+
+    //const data = JSON.parse(event.body);
+    let data;
+    try {
+        data = typeof event.body === "string" ? JSON.parse(event.body) : event.body;
+    } catch (error) {
+        console.error("Error al parsear event.body:", error);
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ message: "Body no es un JSON válido" })
+        };
+    }
+
+    
+    // Inicio - Proteger el Lambda
+    const token = event.headers.Authorization;
+    
+    const payload = JSON.stringify({ token: token });
+    
+    try {
+        const invokeResponse = await lambda.invoke({
+            FunctionName: process.env.VALIDAR_TOKEN_ARN, 
+            Payload: JSON.stringify({ token })
+        }).promise();
+        
+        const response = JSON.parse(invokeResponse.Payload);
+        console.log(response);
+        
+        if (response.statusCode === 403) {
+            return {
+                statusCode: 403,
+                body: JSON.stringify({
+                    status: 'Forbidden - Acceso No Autorizado'
+                })
+            };
+        }
+
+        // Aquí se obtiene el user_id de la respuesta de ValidarTokenAcceso
+        const user_id = response.user_id; 
+        if (!user_id) {
+            return {
+                statusCode: 403,
+                body: JSON.stringify({
+                    status: 'Token inválido o usuario no autorizado'
+                })
+            };
+        }
+
+
+    } catch (error) {
+        console.error("Error invocando la función Lambda: ", error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                status: 'Internal Server Error',
+                message: 'Hubo un problema al validar el token'
+            })
+        };
+    }
+    // Fin - Proteger el Lambda 
+
+
+    // Proceso - Guardar el producto en DynamoDB
+    
+    try {
         
 
-        print(f"Received user_id: {user_id}, password: {password}")
 
+        // Procesar la solicitud de la reseña
+        const item = {
+            id_usuario: user_id,
+            id_resenia: randomUUID(),
+            id_vuelo: data.id_vuelo, //falta validar el id_vuelo
+            calificacion: data.calificacion,
+            comentario: data.comentario,
+            fecha_resena: data.fecha_resena
+        };
 
-        # Verificar que el email y el password existen
-        if user_id and password:
-            # Hashea la contraseña antes de almacenarla
-            hashed_password = hash_password(password)
+        // Guardar la reseña en DynamoDB
+        await dynamodb.put({
+            TableName: REVIEWS_TABLE,
+            Item: item
+        }).promise();
 
-            print(f"Hashed password: {hashed_password}")
-            
-
-
-            # Conectar DynamoDB
-            dynamodb = boto3.resource('dynamodb')
-            table_name = os.environ['tabla_usuarios']
-            t_usuarios = dynamodb.Table(table_name)
-            # Almacena los datos del user en la tabla de usuarios en DynamoDB
-            response = t_usuarios.put_item(
-                Item={
-                    'user_id': user_id,
-                    'password': hashed_password,
-                }
-            )
-
-
-            print(f"Put item response: {response}")  
-
-            # Retornar un código de estado HTTP 200 (OK) y un mensaje de éxito
-            mensaje = {
-                'message': 'User registered successfully',
-                'user_id': user_id
-            }
-            return {
-                'statusCode': 200,
-                'body': json.dumps(mensaje)
-            }
-        else:
-            mensaje = {
-                'error': 'Invalid request body: missing user_id or password'
-            }
-            return {
-                'statusCode': 400,
-                'body': json.dumps(mensaje)
-            }
-
-    except Exception as e:
-        # Excepción y retornar un código de error HTTP 500
-        print("Exception:", str(e))
-        mensaje = {
-            'error': str(e)
-        }        
         return {
-            'statusCode': 500,
-            'body': json.dumps(mensaje)
-        }
+            statusCode: 201,
+            body: JSON.stringify({ 
+                message: 'Reseña creada con éxito',
+                id_resenia: item.id_resenia
+             })
+        };
+    } catch (error) {
+        console.error(error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: 'Ocurrió un error al crear la reseña' })
+        };
+    }
+};
+
