4bb5319d4e0e442791b7ed99dd6d5b5c77fa98af
Jordinn Martin Reyes Medina <113043632+jordinnreyes@users.noreply.github.com>
2024-12-01 20:57:19 -0500

    Update obtenerDestinos.js

--- api-Destinos/obtenerDestinos.js
+++ api-Destinos/obtenerDestinos.js
@@ -1,50 +1,51 @@
 const AWS = require('aws-sdk');
 const dynamodb = new AWS.DynamoDB.DocumentClient();
-const lambda = new AWS.Lambda();
 
 const DESTINOS_TABLE = process.env.DESTINOS_TABLE;
 
 exports.handler = async (event) => {
-
     console.log(event);
-    
 
     let data;
     try {
-        data = typeof event.body === "string" ? JSON.parse(event.body) : event.body;
+        data = event.body ? (typeof event.body === "string" ? JSON.parse(event.body) : event.body) : null;
     } catch (error) {
         return {
             statusCode: 400,
             body: JSON.stringify({
                 message: "Body no es un JSON válido"
-            })
+            }),
         };
     }
-
-    const id_destino = data.id_destino;  // El id_destino que se desea consultar
-    const ciudad = data.ciudad; 
-    // Verificar que el id_destino sea válido
-    if (!id_destino) {
-        return {
-            statusCode: 400,
-            body: JSON.stringify({
-                message: 'El id_destino es obligatorio'
-            })
-        };
-    }
-
 
     try {
-        // Parámetros de escaneo de la tabla
+        // Si no se proporcionan datos en el cuerpo, realizar un escaneo completo
+        if (!data || !data.id_destino) {
+            const params = {
+                TableName: DESTINOS_TABLE,
+            };
+
+            const result = await dynamodb.scan(params).promise();
+
+            return {
+                statusCode: 200,
+                body: JSON.stringify({
+                    message: 'Destinos obtenidos con éxito',
+                    destinos: result.Items,
+                }),
+            };
+        }
+
+        // Si se proporciona un id_destino, obtener un destino específico
+        const { id_destino, ciudad } = data;
         const params = {
             TableName: DESTINOS_TABLE,
             Key: {
-                id_destino: id_destino, 
+                id_destino: id_destino,
                 ciudad: ciudad,
             },
         };
 
-        // Realizar el escaneo para obtener todos los destinos
         const result = await dynamodb.get(params).promise();
 
         if (!result.Item) {
@@ -57,8 +58,8 @@ exports.handler = async (event) => {
         return {
             statusCode: 200,
             body: JSON.stringify({
-                message: 'Destinos obtenidos con éxito',
-                destinos: result.Item,
+                message: 'Destino obtenido con éxito',
+                destino: result.Item,
             }),
         };
     } catch (error) {
