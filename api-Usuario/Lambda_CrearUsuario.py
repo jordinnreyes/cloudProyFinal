42157c22f8513af9140dc9898cda62a0f27dcd19
Jordinn Martin Reyes Medina <113043632+jordinnreyes@users.noreply.github.com>
2024-12-04 23:16:10 -0500

    Update Lambda_CrearUsuario.py

--- api-Usuario/Lambda_CrearUsuario.py
+++ api-Usuario/Lambda_CrearUsuario.py
@@ -11,7 +11,12 @@ def hash_password(password):
 # Función que maneja el registro de user y validación del password
 def lambda_handler(event, context):
     try:
-        body = json.loads(event.get('body', '{}'))  # Esto maneja el caso donde no haya un cuerpo válido
+        #body = json.loads(event.get('body', '{}'))  # Esto maneja el caso donde no haya un cuerpo válido
+        body = event.get('body', '{}')
+        # Si el body no es un diccionario, conviértelo de JSON string a dict
+        if isinstance(body, str):
+            body = json.loads(body)
+
         # Obtener el email y el password
         user_id = body.get('user_id')
         password = body.get('password')
